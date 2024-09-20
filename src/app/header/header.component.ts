1cd6a893515af558102f7e6ff5d737f165315647
bukkuru soma sekhar <bukkuru@esteco.com>
2024-09-20 17:13:57 +0530

    1 user added with interpolation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,13 @@
 import {Component} from "@angular/core";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
+  imports: [
+    NgOptimizedImage
+  ],
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
