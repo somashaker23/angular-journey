aa678c52b0710060073ec3b18b865c845c23728d
bukkuru soma sekhar <bukkuru@esteco.com>
2024-09-23 17:38:20 +0530

    @Input decorator

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,25 +1,21 @@
-import {Component, computed, signal} from '@angular/core';
-import {DUMMY_USERS} from "../dummy.users";
+import {Component, Input} from '@angular/core';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex])
-  imagePath = computed(() => '/assets/users/' + this.selectedUser().avatar)
+  @Input() avatar!: string;
+  @Input({required: true}) name!: string;
 
-  OnSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  get imagePath() {
+    return '/assets/users/' + this.avatar;
   }
 
-  OnHover() {
-    console.log("happy")
+  OnSelectUser() {
+
   }
 }
