169922c9f74abea9bb71b4a8de4f3d32e679f4d3
bukkuru soma sekhar <bukkuru@esteco.com>
2024-09-23 16:24:53 +0530

    Event Binding

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {DUMMY_USERS} from "../dummy.users";
 
-const randomIndex = Math.floor(Math.random() / DUMMY_USERS.length)
+const randomIndex = Math.floor(Math.random() *DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -12,4 +12,17 @@ const randomIndex = Math.floor(Math.random() / DUMMY_USERS.length)
 })
 export class UserComponent {
   selectedUser = DUMMY_USERS[randomIndex]
+
+  get imagePath() {
+    return '/assets/users/' + this.selectedUser.avatar;
+  }
+
+  OnSelectUser() {
+    const randomIndex = Math.floor(Math.random() *DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex];
+  }
+
+  OnHover() {
+    console.log("happy")
+  }
 }
