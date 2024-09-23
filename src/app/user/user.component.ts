03af7b6217fe01ca7c718ba0ad902519443673cf
bukkuru soma sekhar <bukkuru@esteco.com>
2024-09-23 16:59:54 +0530

    intro to signals

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
 import {DUMMY_USERS} from "../dummy.users";
 
-const randomIndex = Math.floor(Math.random() *DUMMY_USERS.length)
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -11,15 +11,12 @@ const randomIndex = Math.floor(Math.random() *DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex]
-
-  get imagePath() {
-    return '/assets/users/' + this.selectedUser.avatar;
-  }
+  selectedUser = signal(DUMMY_USERS[randomIndex])
+  imagePath = computed(() => '/assets/users/' + this.selectedUser().avatar)
 
   OnSelectUser() {
-    const randomIndex = Math.floor(Math.random() *DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 
   OnHover() {
