0ec69a01546b34d2a501450faf9f3aacb1e5781c
bukkuru soma sekhar <bukkuru@esteco.com>
2024-09-25 17:06:46 +0530

    more components added to create initial UI structure.

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,9 +1,11 @@
 import {Component, Input} from "@angular/core";
+import {TaskComponent} from "./task/task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
+  imports: [TaskComponent],
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
