c5bbe09da9e4f811405e20dd246741b7a3914363
bukkuru soma sekhar <bukkuru@esteco.com>
2024-09-30 17:01:58 +0530

    using ng-content

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,15 @@
 import {Component, EventEmitter, Input, Output} from "@angular/core";
 import {type User} from "./user.model";
+import {CardComponent} from "../shared/card/card.component";
 
 
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
+  imports: [
+    CardComponent
+  ],
   styleUrl: './user.component.css'
 })
 export class UserComponent {
